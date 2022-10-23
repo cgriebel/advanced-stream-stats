3720f1391462f4315b548565db81fbce6879ae11
Chris Griebel <chris.griebel.dev@gmail.com>
2022-10-23 15:24:53 -0400

    Added fix for net::ERR_ADDRESS_INVALID

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    // required to prevent errors calling 0.0.0.0 on windows in docker
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
